cd64a1d986c93e74c2308bc099b1e90027f05dbe
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-07-05 21:57:01 +0200

    Added linting config to backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,24 +1,22 @@
+
+/* eslint-disable no-undef */
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'prettier/@typescript-eslint',
-  ],
-  root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
-  },
+	parser: '@typescript-eslint/parser',  // Specifies the ESLint parser
+	extends: [
+		"eslint:recommended",             // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+		"plugin:@typescript-eslint/eslint-recommended",
+		"plugin:@typescript-eslint/recommended"
+	],
+	parserOptions: {
+		ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
+		sourceType: 'module',  // Allows for the use of imports
+	},
+	rules: {
+		"semi": ["error","always"],
+		"indent": ["error", "tab"],
+		"prefer-const": ["error",{}],
+		"max-len": ["error", {code: 140, ignoreComments: true}]
+		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+		// e.g. "@typescript-eslint/explicit-function-return-type": "off",
+	},
 };
